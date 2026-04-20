b911ccd23b702b9a82e715ece14cdf2b2707fe4d
Tsphreres <onesphore200@gmail.com>
2026-04-20 15:25:12 +0300

    Create menu.php

---
+++ project3/menu/menu.php
@@ -0,0 +1,20 @@
+<h1>Our Menu - Ibiryo byacu</h1>
+<div class="menu-table">
+    <table>
+        <thead>
+            <tr><th>Item</th><th>Description</th><th>Price (RWF)</th><th>Action</th></tr>
+        </thead>
+        <tbody>
+            <tr><td>Ubugali n"iNyama</td><td>Traditional cornmeal with grilled goat meat</td><td class="price">5,000 RWF</td><td><a href="index.php?page=order&item=Ubugali%20n%22iNyama" class="order-link">Order Now</a></td></tr>
+            <tr><td>Brochettes</td><td>Beef brochettes with spicy sauce (6 pieces)</td><td class="price">4,500 RWF</td><td><a href="index.php?page=order&item=Brochettes" class="order-link">Order Now</a></td></tr>
+            <tr><td>Igitoki</td><td>Steamed green bananas with beef stew</td><td class="price">6,000 RWF</td><td><a href="index.php?page=order&item=Igitoki" class="order-link">Order Now</a></td></tr>
+            <tr><td>Isombe</td><td>Cassava leaves with peanuts and rice</td><td class="price">4,000 RWF</td><td><a href="index.php?page=order&item=Isombe" class="order-link">Order Now</a></td></tr>
+            <tr><td>Akabenz&Ifi</td><td>Grilled pork with fried plantains</td><td class="price">7,000 RWF</td><td><a href="index.php?page=order&item=Akabenz%26Ifi" class="order-link">Order Now</a></td></tr>
+            <tr><td>Rwandan Coffee</td><td>Premium Arabica coffee from Lake Kivu</td><td class="price">2,000 RWF</td><td><a href="index.php?page=order&item=Rwandan%20Coffee" class="order-link">Order Now</a></td></tr>
+            <tr><td>Passion Juice</td><td>Fresh passion fruit juice</td><td class="price">2,500 RWF</td><td><a href="index.php?page=order&item=Passion%20Juice" class="order-link">Order Now</a></td></tr>
+        </tbody>
+    </table>
+</div>
+<div class="rwandan-note" style="margin-top: 1rem;">
+    All prices in Rwandan Francs (RWF). Taxes included. Serving authentic Rwandan cuisine!
+</div>
